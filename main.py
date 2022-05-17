39ff375a5ae7cd7e08524b6d3c8d18d70f40e61b
Mwalters75 <808426@pdsb.net>
2022-05-17 09:34:55 -0400

    Fix some more stuff.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def getupdate(): # update
 	prevdir = os.getcwd()
 	os.chdir(f"{homedir}/tmp")
 	print("Step 1/3 complete.")
-	os.system(f"curl -s '{getrepo}moexe/list' -O")
+	os.system(f"curl -s {getrepo}moexe/list -O")
 	filepath = f"{homedir}/tmp/list"
 	with open(filepath) as fp:
 		for index, line in enumerate(fp):
